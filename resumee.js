d6b2468e26eb52d9f6a2f172b0e446aac67b5946
ASFER8384 <115686272+ASFER8384@users.noreply.github.com>
2022-10-19 08:50:55 -0700

    Add files via upload

---
+++ resumee.js
@@ -0,0 +1,40 @@
+let resume=[{
+    "Tittle"  : "BIODATA",
+    "Name"    : "Mohamed Asfer Ali",
+    "Address" : "4/185 Periyakadai Street Devipattinam",
+    "Distric" : "Ramnad",
+    "Pincode" : "623514",
+    "Contact" : "8220958384",
+    "Gmail"   : "asferali8384@gmail.com"
+},
+{
+    "Course"     : "ACADEMIC QUALIFICATION",
+
+    "B.E(mech)"  : "Alameen Engineering College - 80%",
+    "HSC"        : "Bukhariya Matric Hrc School - 77%",
+    "SSLC"       : "Bukhariya Matric Hrc School - 60%",
+},
+{
+    "Personal"   : "SKILLS",
+    "Designing"  : "Auto Cad",
+    "Mechanical" : "CNC Operator",
+    "Technical " : "MS Offic"
+},
+{
+    "Project Tittle" : "DESCRIPTION",
+    "Design and Fabrication " : "Design and Fabrication of Pneumatic Sheet Metal Cutting and Punching Machine",
+
+},
+{
+    "CERTIFICATES" : "ACHIEVEMENTS",
+    "Valuthur Gas Turbine Power Plant(govt) " : "In-Plant Training in Valuthur  Gas Turbine Power Plant ",
+
+    
+},
+{
+    "PERSONAL"    : "PROFILE",
+    "Father Name" : "Nagoor Meeran",
+    "DOB"         : "21-02-2001",
+    "Age"         : "21"
+    
+}]
